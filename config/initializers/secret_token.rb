04581b0f507750824d1fb37bacf9c1561e8f11e1
Shayan <palmtreetutoring@gmail.com>
2014-01-19 14:33:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jackal::Application.config.secret_key_base = 'ea9ac64eb49c07414bca3bae359b71ce8557837192273358bf39a10d5ec619248157e3b956d907bc7facfcaee571d8de34e0addf8e8c128aca0d49d0e7e85fa1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jackal::Application.config.session_store :cookie_store, key: '_jackal_session'
